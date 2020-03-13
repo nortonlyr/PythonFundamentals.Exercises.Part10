ace16e02c596eaa80b742d5c3f316eb48d649f00
Norton Li <nli@zipcodes-MBP.home>
2020-03-13 00:52:33 -0400

    1st draft

--- small_town_teller.py
+++ small_town_teller.py
@@ -1,14 +1,14 @@
-class Person():
-	def __init__(self, id, first_name, last_name):
-		self.id = id
+class Person:
+	def __init__(self, id_, first_name, last_name):
+		self.id_ = id_
 		self.first_name = first_name
 		self.last_name = last_name
 
 	def unique_id(id):
-		for s in id:
+		for s in id_:
 
 
-class Account():
+class Account:
 	def __init__(self, number, type_, owner, balance):
 		self.number = number
 		self.type_ = type_
@@ -18,15 +18,25 @@ class Account():
 
 class Bank:
 	def __init__(self):
-		self.add_customer = add_customer()
+		# self.add_customer = add_customer
 
-		self.add_account = add_account()
-		self.remove_account = remove_account()
-		self.deposit = deposit()
-		self.withdraw = withdraw()
-		self.balance_inquiry = balance_inquiry()
+		# self.add_account = add_account
+		# self.remove_account = remove_account
+		# self.deposit = deposit
+		# self.withdraw = withdraw
+		# self.balance_inquiry = balance_inquiry
 
 	# def of money deposit
+	def add_customer(self):
+		add_customer.append(id_)
+		add_customer.append(first_name)
+		add_customer.append(last_name)
+
+	def add_account(self):
+		add_account.append(number)
+		add_account.append(type_)
+		add_account.append(owner)
+
 	def deposit(self):
 		amount = float(input('Please enter the deposit amount:'))
 		self.balance += amount
