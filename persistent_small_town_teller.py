6e82adc5fed591e6b843eca6b6b8edc7ae789300
Norton Li <nli@zipcodes-MBP.home>
2020-03-13 23:23:42 -0400

    Done Ex1

---
+++ persistent_small_town_teller.py
@@ -0,0 +1,17 @@
+import pickle
+
+class PersistenceUtils:
+
+    def write_pickle(self):
+        num_of_acc_data =
+        for i in range(acc_data):
+
+    with open ('filename.pickle', 'wb') as acc_file:
+        pickle.dump(data, acc_file)
+        acc_file.close()
+
+    def load_pick(self):
+        with open ('filename.pickle', 'rb') as acc_file:
+        pickle.load(acc_file)
+        acc_file.close()
+
--- small_town_teller.py
+++ small_town_teller.py
@@ -1,63 +1,80 @@
+cust_data = {}
+acc_data = {}
+acc_type = ['Checking', 'Savings']
+
 class Person:
-	def __init__(self, id_, first_name, last_name):
-		self.id_ = id_
-		self.first_name = first_name
+	def __init__(self, id, first_name, last_name):
+		self.id = id,
+		self.first_name = first_name,
 		self.last_name = last_name
 
-	def unique_id(id):
-		for s in id_:
+	def get_id(self):
+		return self.id_
 
+	def get_first_name(self):
+		return self.first_name
+
+	def get_last_name(self):
+		return self.last_name
 
 class Account:
-	def __init__(self, number, type_, owner, balance):
-		self.number = number
-		self.type_ = type_
-		self.owner = owner
+	def __init__(self, acc_number, acc_type, owner, balance = 0):
+		self.acc_number = acc_number,
+		self.acc_type = acc_type,
+		self.owner = owner,
 		self.balance = balance
 
-
-class Bank:
+class Bank(Person, Account):
 	def __init__(self):
-		# self.add_customer = add_customer
+		pass
 
-		# self.add_account = add_account
-		# self.remove_account = remove_account
-		# self.deposit = deposit
-		# self.withdraw = withdraw
-		# self.balance_inquiry = balance_inquiry
+	def add_customer(self, id, first_name, last_name):
+		if id in cust_data:
+			print('ID already exists')
+		else:
+			cust_data[id] = first_name + " " + last_name
+			print(f'Welcome {first_name + " " + last_name}, you new account ID is {id}')
 
-	# def of money deposit
-	def add_customer(self):
-		add_customer.append(id_)
-		add_customer.append(first_name)
-		add_customer.append(last_name)
+	def add_account(self, id, acc_number, type):
+		if id in cust_data and type == 'Checking':
+			acc_data[id] = {acc_number:type}
+			print(f'{cust_data[id]}, your {acc_type[0]} account is created.')
+		else:
+			id in cust_data and type == 'Savings'
+			acc_data[id] = {acc_number:type}
+			print(f'{cust_data[id]}, your {acc_type[1]} account is created.')
 
-	def add_account(self):
-		add_account.append(number)
-		add_account.append(type_)
-		add_account.append(owner)
+	def remove_account(self, account):
+		pass
 
 	def deposit(self):
 		amount = float(input('Please enter the deposit amount:'))
 		self.balance += amount
-		print('Deposit amount:' + amount)
+		print(f'Deposit amount: {amount}')
 
 	def withdraw(self):
 		amount = float(input('Please enter the withdraw amount:'))
-		if self.balance >= amount
+		if self.balance >= amount:
 			self.balance -= amount
-			print('Your withdrwa amount:' + amount)
-		else
+			print(f'Your withdrawal amount is {amount}')
+		else:
 			print('Insufficient fund')
 
 	def balance_inquiry(self):
-		print('Your current balance:' + self.balance)
+		print(f'Your available balance: {self.balance}')
+
+
+cust_bank = Bank()
+cust_bank.add_customer(1, 'Norton', 'Lee')
+cust_bank.add_customer(2, 'Jacky', 'Robinson')
+cust_bank.add_account(1, 1000, 'Checking')
+cust_bank.add_account(2, 560, 'Savings')
 
 
 """
 Constraints
-
 When attempting to register a customer, the customer id must be unique.
 When attempting to add an account, the user associated with said account 
 must already registered as a customer.
 When attempting to add an account, the account number must be unique.
+"""
