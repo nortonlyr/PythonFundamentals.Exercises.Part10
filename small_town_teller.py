6d6a06dcd6bc30aee31162512b567d20320adbff
Norton Li <nli@zipcodes-MBP.home>
2020-03-17 21:12:11 -0400

    Lab updated

--- small_town_teller.py
+++ small_town_teller.py
@@ -1,69 +1,67 @@
+from typing import Dict
+
 class Person:
-	def __init__(self, id, first_name, last_name):
-		self.id = id
+
+	def __init__(self, person_id, first_name, last_name):
+		self.id = person_id
 		self.first_name = first_name
 		self.last_name = last_name
+	def __str__(self):
+		return f"id: {self.id}. owner: {self.first_name} {self.last_name}"
 
 class Account:
-	def __init__(self, number, type, owner, balance = 0):
-		self.number = number
-		self.type = type
-		self.owner = owner
-		self.balance = balance
+
+	def __init__(self, acct_number, acct_type, acct_owner, balance = 0):
+		self.number = acct_number
+		self.type = acct_type
+		self.owner = acct_owner
+		self.balance = 0
+	def __str__(self):
+		return f"number: {self.number}. type: {self.type}. balance: {self.balance}"
+
 
 class Bank:
-	cust_data = []
-	acc_data = {}
-	acc_type = ['Checking', 'Savings']
 
-	def add_customer(self, customer):
-		if customer.id in self.cust_data:
-			print('ID already exists')
+	def __init__(self):
+		self.customers: Dict[int, Person] = dict()
+		self.accounts: Dict[int, Account] = dict()
+
+	def add_customer(self, customer: Person) -> None:
+		if customer.id in self.customers:
+			raise ValueError (f"Customer with id {customer.id} already exist.")
 		else:
-			self.cust_data.append(customer.id)
+			self.customers[customer.id] = customer
 
-	def add_account(self, account):
-		if account not in self.acc_data:
-			self.acc_data[account.number] = account.balance
-
-	def remove_account(self, account):
-		if account in self.acc_data:
-			del[self.acc_data[account]]
-
-	def deposit(self, account, amount):
-		self.acc_data[account] += amount
-
-
-	def withdraw(self, account, amount):
-		if self.acc_data[account] >= amount:
-			self.acc_data[account] -= amount
+	def add_account(self, account: Account):
+		if account.owner.id not in self.customers:
+			raise ValueError(f"{account.owner.id} is not a valid customer id.")
+		elif account.number in self.accounts:
+			raise ValueError(f"Account with id {account.number} already exists")
 		else:
-			print('Insufficient fund')
+			self.accounts[account.number] = account
 
-	def balance_inquiry(self, account):
-		print(f'Your available balance: {self.acc_data[account]}')
+	def remove_account(self, account_id):
+		if account_id in self.accounts:
+			del self.accounts[account_id]
 
+	def deposit(self, account_id: int, amount: float):
+		if account_id in self.accounts:
+			account = self.accounts.get(account_id)
+			account.balance += round(amount, 2)
+		else:
+			raise ValueError(f"Account with id {account_id} does not exist.")
 
-cust_bank = Bank()
-Norton = Person(10101, 'Norton', 'Lee')
+	def withdraw(self, account_id, amount: float):
+		if account_id in self.accounts:
+			account = self.accounts.get(account_id)
+			account.balance -= round(amount, 2)
 
-cust_bank.add_customer(Norton)
-print(cust_bank.cust_data)
-
-
-Norton_Checking = Account(7000, 'Checking', Norton)
-print(cust_bank.acc_data)
-
-cust_bank.add_account(Norton_Checking)
-print(cust_bank.acc_data)
-
-cust_bank.deposit(7000, 10000)
-print(cust_bank.acc_data)
-
-cust_bank.withdraw(7000, 4000)
-print(cust_bank.acc_data)
-
-cust_bank.balance_inquiry(7000)
+	def balance_inquiry(self, account_id: int):
+		if account_id in self.accounts:
+			balance = self.accounts.get(account_id).balance
+			return round(balance, 2)
+		else:
+			raise ValueError (f" Account with id {account_id} does not exist.")
 
 
 """
